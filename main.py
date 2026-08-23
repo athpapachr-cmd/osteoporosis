393661ba58a5fcc693094c4b8fd862c42786023c
athpapachr-cmd <athpapachr@gmail.com>
2026-08-23 08:42:51 +0300

    feat: add cookie auth for clinical routes

--- main.py
+++ main.py
@@ -6,9 +6,12 @@ patient-centric clinical-data routers used by the Clinical Excellence baseline U
 """
 
 from legacy_main import app, engine
+from clinical_auth import ClinicalCookieMiddleware, build_auth_router
 from clinical_data import build_clinical_router
 from clinical_data_ext import build_clinical_ext_router
 
+app.add_middleware(ClinicalCookieMiddleware)
+app.include_router(build_auth_router())
 app.include_router(build_clinical_router(engine))
 app.include_router(build_clinical_ext_router(engine))
 
